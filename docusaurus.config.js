477f86bc74cbae617ba40502b17ee8bf2c8db6fb
Elias Nau <elias@codity.net>
2025-02-16 16:34:41 +0100

    Deploy website - based on 26e852e484ed6895f458d51fc7c8edb17f7e02c8

--- docusaurus.config.js
+++
@@ -1,6 +0,0 @@
-// ... existing config ...
-module.exports = {
-  // ... other config options ...
-  onBrokenLinks: 'warn', // Change this temporarily to 'warn' instead of 'throw'
-  // ... rest of config
-}
